c50f8a8f13cf4d0fb523f5d188f91353926feeb4
Hanbee Lee <bee1162@naver.com>
2022-09-30 20:21:41 +0900

    :feat:필요없는 인풋값들 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import InputForm from './components/InputMessage';
 import styled from 'styled-components';
-import { text } from 'stream/consumers';
+
 import MessageChatForm from './components/MessageChatForm';
 
 import { useRecoilState, useRecoilValue, RecoilRoot } from 'recoil';
-import useInput from './hooks/useInput';
+
 import useChatRoom from './hooks/useChatRoom';
 import { useEffect } from 'react';
 import { chatRoomState } from './atom';
