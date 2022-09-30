6c39003939bf5c3c73bea19e85694fe9e28732cf
Yoo SeonHo <tjsgh503@naver.com>
2022-09-30 22:24:54 +0900

    feat : add json and atom default

--- src/state/state.tsx
+++ src/state/state.tsx
@@ -1,9 +1,10 @@
 import { atom } from 'recoil';
 import { ListInfo } from '../interfaces/interface';
+import Chatting from '../json/default.json';
 
 export const listState = atom<ListInfo[]>({
   key: 'List',
-  default: [],
+  default: Chatting.ChattingList,
 });
 
 export const textState = atom<string>({
