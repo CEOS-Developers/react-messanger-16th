8809714785c49b010cff40c15805896c43a3364d
Jieun Oh <65931227+0909oje@users.noreply.github.com>
2022-10-02 14:12:36 +0900

    fix : change any type to chatting[]

--- src/recoil/store.tsx
+++ src/recoil/store.tsx
@@ -1,12 +1,13 @@
 import { atom } from 'recoil';
 import chatInfo from '../assets/chatInfo.json';
+import { chatting } from '../interface/chatting';
 
 export const activeId = atom<number>({
   key: 'activeId',
   default: 0,
 });
 
-export const chatList = atom<any>({
+export const chatList = atom<chatting[]>({
   key: 'chatList',
   default: chatInfo,
 });
