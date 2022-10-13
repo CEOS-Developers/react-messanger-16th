6ff33a88d721d848ca8b1905c197dc21e336e551
heesoo <11junghs@gmail.com>
2022-10-13 17:46:04 +0900

    feat : 페이지 구분

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
+import Friends from "./pages/Friends";
+import Chat from "./pages/Chat";
+import Setting from "./pages/Setting";
+
 function App() {
-  return <div>화이팅!! 4주차</div>;
+  return (
+    <Routes>
+      <Route path = '/' element={<Friends/>}/>
+      <Route path = '/chat' element={<Chat/>}/>
+      <Route path = '/setting' element={<Setting/>}/>
+    </Routes>
+  );
 }
 
 export default App;
