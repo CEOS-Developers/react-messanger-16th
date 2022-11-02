ae76f1ed5a2295d186425d9733e44adc115620cd
heesoo <11junghs@gmail.com>
2022-11-02 16:05:57 +0900

    feat: setting에 github 링크 추가

--- src/pages/Setting/Setting.tsx
+++ src/pages/Setting/Setting.tsx
@@ -1,6 +1,24 @@
 import React from 'react';
 import styled from "styled-components"
 import Header from '../../components/Header';
+import { Link } from 'react-router-dom';
+
+const Setting = () => {
+    return (
+        <StyledBlock>
+            <Header/>
+            <StyledContent>
+                더보기  
+                <hr/>
+                <a href="https://github.com/heeeesoo/react-messanger-16th/tree/heeeesoo2">
+                    <StyledSetting>
+                        Github
+                    </StyledSetting>
+                </a>
+            </StyledContent>
+        </StyledBlock>
+    );
+};
 
 const StyledBlock = styled.div`
     display: flex;
@@ -18,15 +36,14 @@ const StyledContent = styled.div`
     border-radius:0px 30px 30px 0px;
     margin: 20px;
 `
-const Setting = () => {
-    return (
-        <StyledBlock>
-            <Header/>
-            <StyledContent>
-                더보기    
-            </StyledContent>
-        </StyledBlock>
-    );
-};
+
+const StyledSetting = styled.div`
+    text-decoration: none;
+    margin: 10px;
+    width: auto;
+    height: auto;
+    font-size: 15px;
+    color: gray;
+`
 
 export default Setting;
