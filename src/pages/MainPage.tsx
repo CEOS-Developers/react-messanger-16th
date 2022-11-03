130d57401ec665ecfc068a0924c4d48c214ee827
강나연 <seoultownroad@naver.com>
2022-11-04 03:43:16 +0900

    :speech_balloon: feat: Set MainPage

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,7 +3,12 @@ import { Link } from "react-router-dom";
 const MainPage = () => {
   return (
     <MainPageContainer>
-      <Link to={`/friends`}> hi </Link>
+      <Link to={`/friends`}>
+        <MainPageImage src={`${process.env.PUBLIC_URL}/images/mainPage.png`} />
+        <ButtonWrapper>
+          <Button>click to start</Button>
+        </ButtonWrapper>
+      </Link>
     </MainPageContainer>
   );
 };
@@ -17,5 +22,34 @@ const MainPageContainer = styled.div`
   background: linear-gradient(#bed7f9, white 30%, white 90%, #bed7f9);
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
+  justify-content: center;
+  align-items: center;
+`;
+
+const MainPageImage = styled.img`
+  width: 100%;
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  align-items: center;
+  flex-direction: column;
+  padding: 0;
+`;
+
+const Button = styled.button`
+  display: flex;
+  border-radius: 50px;
+  border: 1px solid #000;
+
+  font-size: 2rem;
+  height: 3.5rem;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 2rem;
+  margin: 1.3rem;
+
+  background: linear-gradient(180deg, #ffffff 0%, #aeff9c 100%);
 `;
