fba91c7d94398f93e9fffa8bb02723923d92e5ea
hamo-o <yheel369@gmail.com>
2022-11-04 19:59:28 +0900

    design: font 저장

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -3,9 +3,19 @@ import reset from './reset.css';
 
 const GlobalStyle = createGlobalStyle`
 ${reset};
+@font-face {
+  font-family: 'Pretendard-Regular';
+  src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff')
+    format('woff');
+  font-weight: 400;
+  font-style: normal;
+}
+
 html {
     width: 100vw;
     height: 100vh;
+
+    font-family: 'Pretendard-Regular';
   }
 
 body {
