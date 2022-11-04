7b342fb275a50fbfc80701c5932d4e6f56aa574b
seondal <sseondal@gmail.com>
2022-11-04 16:30:16 +0900

    [Style] Layout NavLink

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,12 +1,35 @@
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 const Layout = () => {
+  const activeStyle = {
+    color: "pink",
+  };
+
+  const normalStyle = {
+    color: "black",
+  };
+
   return (
     <div>
       <div>
-        <Link to="/">친구</Link>
-        <Link to="/chatrooms">채팅</Link>
-        <Link to="/setting">설정</Link>
+        <NavLink
+          to="/"
+          style={({ isActive }) => (isActive ? activeStyle : normalStyle)}
+        >
+          친구
+        </NavLink>
+        <NavLink
+          to="/chatrooms"
+          style={({ isActive }) => (isActive ? activeStyle : normalStyle)}
+        >
+          채팅
+        </NavLink>
+        <NavLink
+          to="/setting"
+          style={({ isActive }) => (isActive ? activeStyle : normalStyle)}
+        >
+          설정
+        </NavLink>
         <Outlet />
       </div>
     </div>
