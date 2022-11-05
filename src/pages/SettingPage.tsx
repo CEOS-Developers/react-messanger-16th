79b63bb45ce15820adaa4076d77843aca7e785a7
seondal <sseondal@gmail.com>
2022-11-06 00:16:19 +0900

    [Feat] SettingPage

--- src/pages/SettingPage.tsx
+++ src/pages/SettingPage.tsx
@@ -1,7 +1,10 @@
 const SettingPage = () => {
   return (
     <div>
-      <h1>설정 더보기</h1>
+      <a href="https://github.com/seondal/react-messanger-16th">GitHub Repo</a>
+      <a href="https://velog.io/@seondal/React-%EC%99%80-Typescript-%EC%9D%B4%EC%9A%A9%ED%95%98%EC%97%AC-%EC%B9%B4%EC%B9%B4%EC%98%A4%ED%86%A1-%ED%81%B4%EB%A1%A0%EC%BD%94%EB%94%A9">
+        Velog
+      </a>
     </div>
   );
 };
