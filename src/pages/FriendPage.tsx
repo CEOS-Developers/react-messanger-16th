32be316d250343dadd999f3b507a0ff66040e2c0
seondal <sseondal@gmail.com>
2022-11-05 22:41:11 +0900

    [Feat] FriendPage 유저 리스트 띄우기

--- src/pages/FriendPage.tsx
+++ src/pages/FriendPage.tsx
@@ -1,7 +1,18 @@
+import userData from "../data/userData.json";
+import { ProfileImage } from "../common/StyledComponents";
+
 const FriendPage = () => {
+  const users = userData.users;
+
   return (
     <div>
-      <h1>친구목록</h1>
+      {users.map((user) => (
+        <div>
+          <ProfileImage src={`${user.profileImage}`} />
+          <span>{user.name}</span>
+          <div>{user.message}</div>
+        </div>
+      ))}
     </div>
   );
 };
